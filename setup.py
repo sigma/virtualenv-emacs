abf7fbebcba4df8ba9268160637e465f54b990de
Yann Hodique <yann.hodique@gmail.com>
2013-02-24 23:38:53 +0000

    fix packaging issue

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(name="virtualenv-emacs",
-      version='0.1',
+      version='0.1.1',
       author='Yann Hodique',
       author_email='yann.hodique@gmail.com',
       url='http://github.com/sigma/virtualenv-emacs',
       scripts=['virtualenv_install_emacs',],
       packages=['virtualenv_emacs'],
       package_data={'virtualenv_emacs': ['lisp/*.el']},
+      include_package_data=True,
       install_requires=['setuptools']
   )
