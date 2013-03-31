213ceffcb638d9844eef3d14481ab0a57f1abefa
Yann Hodique <yann.hodique@gmail.com>
2013-03-31 19:57:47 +0100

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="virtualenv-emacs",
-      version='0.1.4',
+      version='0.1.5',
       author='Yann Hodique',
       author_email='yann.hodique@gmail.com',
       url='http://github.com/sigma/virtualenv-emacs',
