3015917011ef6af15552d05a9545aef00e7dd0c6
Yann Hodique <yann.hodique@gmail.com>
2013-03-31 19:57:37 +0100

    also install ert-x.el

--- virtualenv_emacs/lisp/bootstrap-package.el
+++ virtualenv_emacs/lisp/bootstrap-package.el
@@ -1,8 +1,14 @@
+;; Some packages are only available in marmalade for now
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 (package-refresh-contents)
 
+;; let's update package.el if need be, as the one we use for bootstrap is
+;; minimal and outdated
 (package-install 'package)
+
+;; a couple of standard packages. This is mostly for emacs 23
 (package-install 'ert)
+(package-install 'ert-x)
