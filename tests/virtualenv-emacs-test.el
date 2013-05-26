82d1c5c02c99d8e2bf474c90498dbc26a521bdcf
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-26 20:18:37 +0200

    Test package-install-file

---
+++ tests/virtualenv-emacs-test.el
@@ -0,0 +1,40 @@
+;;; virtualenv-emacs-test.el --- Dummy package for test
+
+;; Copyright (C) 2013 Takafumi Arakaki
+
+;; Author: Takafumi Arakaki <aka.tkf at gmail.com>
+;; Package-Requires: ((mocker "0.2.6"))
+;; Version: 0.1.5
+
+;; This file is NOT part of GNU Emacs.
+
+;; virtualenv-emacs-test.el is free software: you can redistribute it
+;; and/or modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; virtualenv-emacs-test.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with virtualenv-emacs-test.el.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'mocker)
+
+(provide 'virtualenv-emacs-test)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; virtualenv-emacs-test.el ends here
