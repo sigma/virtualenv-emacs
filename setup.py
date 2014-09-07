6bcc9be4e5fd07ba92509e77d41ceacd3ea4d025
Low Kian Seong <kianseong@gmail.com>
2014-09-07 12:26:07 +0800

    Update setup.py
    Hi, came across your project and it was listed as `UNKNOWN` for description on `pypi` so I added a `description` string.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(name="virtualenv-emacs",
       version='0.1.5',
       author='Yann Hodique',
       author_email='yann.hodique@gmail.com',
+      description=('Helper to injecting a custom version of emacs '
+                   'with package.el enabled into an existing virtualenv'),
       url='http://github.com/sigma/virtualenv-emacs',
       scripts=['virtualenv_install_emacs',],
       packages=['virtualenv_emacs'],
